3681d0c8bd04602e2694d30e87812a51cbc1528c
gervit1989 <gervit2008@mail.ru>
2025-12-07 22:19:56 +0300

    Добавили блоки user и shop

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #=======my-app=============
+    'rest_framework',
+    'user',
+    'shop',
 ]
 
 MIDDLEWARE = [
